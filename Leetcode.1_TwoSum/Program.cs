c52738406bd3ff338b69e53cc75a0b410dae3016
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-05-03 13:04:45 +0200

    Leetcode.1_TwoSum

--- Leetcode.1_TwoSum/Program.cs
+++ Leetcode.1_TwoSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Leetcode._1_TwoSum
 {
@@ -7,6 +8,38 @@ namespace Leetcode._1_TwoSum
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int[] TwoSum_1(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            throw new Exception("Not found");
+        }
+
+        public static int[] TwoSum_2(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for(var i = 0; i < nums.Length; i++)
+            {
+                if(dictionary.ContainsKey(target - nums[i]))
+                {
+                    return new[] { dictionary[target - nums[i]], i };
+                }
+
+                dictionary.Add(nums[i], i);
+            }
+
+            throw new Exception("Not found");
         }
     }
 }
