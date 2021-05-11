d8559b52a7e32e3a9ceaf2e09c5f25a26d1b4a2d
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-05-11 23:18:27 +0200

    RemoveElement

---
+++ Leetcode.27_RemoveElement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Leetcode._27_RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = RemoveElement(new int[] { 3, 2, 2, 3 }, 3);
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            var i = 0;
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+
+            return i;
+        }
+    }
+}
