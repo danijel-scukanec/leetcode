f89746f629a4fe38c0cfa1140e25a84448037de1
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-05-17 00:58:52 +0200

    LengthOfLastWord

---
+++ Leetcode.58_LengthOfLastWord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leetcode._58_LengthOfLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LengthOfLastWord(string s)
+        {
+            var lastWordLength = 0;
+            for(var i = s.Length - 1; i >= 0; i--)
+            {
+                if(s[i] == ' ' && lastWordLength == 0)
+                {                    
+                    continue;
+                }
+                else if(s[i] == ' ' && lastWordLength > 0)
+                {
+                    break;
+                }
+
+                lastWordLength++;
+            }
+
+            return lastWordLength;
+        }
+    }
+}
