603d647df6f9b14a828cde61ef6f14f7ad8adda0
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-05-18 01:11:37 +0200

    ClimbingStairs

---
+++ Leetcode.66_PlusOne/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leetcode._66_PlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PlusOne(new[] { 1, 2, 3 }));
+            Console.WriteLine(PlusOne(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }));
+        }
+
+        public static int[] PlusOne(int[] digits)
+        {
+            List<int> result;
+
+            for (var i = digits.Length; i >= 0; i--)
+            {
+                if (digits[i] == 9 )
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        digits[i]++;
+                        if (i + 1 < digits.Length)
+                        {
+                            i++;
+                            while (i < digits.Length)
+                            {
+                                digits[i] = 0;
+                            }
+                        }
+
+                        result = digits.ToList();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Leetcode.70_ClimbingStairs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Leetcode._70_ClimbingStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(1));
+            Console.WriteLine(ClimbStairs(3));
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            int[] memo = new int[n + 1];
+            return ClimbStairsRecursive(0, n, memo);
+        }
+
+        public static int ClimbStairsRecursive(int i, int n, int[] memo)
+        {
+            if (i > n)
+            {
+                return 0;
+            }
+            if (i == n)
+            {
+                return 1;
+            }
+            if (memo[i] > 0)
+            {
+                return memo[i];
+            }
+            memo[i] = ClimbStairsRecursive(i + 1, n, memo) + ClimbStairsRecursive(i + 2, n, memo);
+            return memo[i];
+        }
+    }
+}
