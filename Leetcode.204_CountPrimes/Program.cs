32cc60d4c60555fd81a2250f256aceaa3680fd72
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-05-23 12:37:40 +0200

    CountPrimes

---
+++ Leetcode.204_CountPrimes/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode._204_CountPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPrimes_1(10));
+            Console.WriteLine(CountPrimes_2(10));
+        }
+
+        public static int CountPrimes_1(int n)
+        {
+            if (n <= 2)
+            {
+                return 0;
+            }
+
+            var counter = 1;
+            var hashset = new HashSet<int>();
+
+            for (var currentPrime = 2; currentPrime < n;)
+            {
+                var currentNumber = currentPrime + currentPrime;
+                hashset.Add(currentNumber);
+
+                while (currentNumber < n)
+                {
+                    currentNumber += currentPrime;
+                    hashset.Add(currentNumber);
+                }
+
+                currentPrime++;
+                while (hashset.Contains(currentPrime))
+                {
+                    currentPrime++;
+                }
+
+                if (currentPrime < n)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public static int CountPrimes_2(int n)
+        {
+            if (n <= 2)
+            {
+                return 0;
+            }
+
+            bool[] numbers = new bool[n];
+            for (int p = 2; p <= (int)Math.Sqrt(n); ++p)
+            {
+                if (numbers[p] == false)
+                {
+                    for (int j = p * p; j < n; j += p)
+                    {
+                        numbers[j] = true;
+                    }
+                }
+            }
+
+            int numberOfPrimes = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (numbers[i] == false)
+                {
+                    ++numberOfPrimes;
+                }
+            }
+
+            return numberOfPrimes;
+        }
+    }
+}
---
+++ Leetcode.31_NextPermutation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Leetcode._31_NextPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NextPermutation(new[] { 4, 2, 0, 2, 3, 2, 0 });
+            NextPermutation(new[] { 1, 2, 3 });
+            NextPermutation(new[] { 3, 2, 1 });
+            NextPermutation(new[] { 1, 1, 5 });
+            NextPermutation(new[] { 1, 3, 2 });
+            NextPermutation(new[] { 1 });
+        }
+
+        public static void NextPermutation(int[] nums)
+        {
+            if (nums.Length <= 1)
+            {
+                return;
+            }
+
+            var leftPointer = nums.Length - 2;
+            var rightPointer = nums.Length - 1;
+
+            while(leftPointer < rightPointer)
+            {
+                if(nums[leftPointer] < nums[rightPointer])
+                {
+                    var tmp = nums[leftPointer];
+                    nums[leftPointer] = nums[rightPointer];
+                    nums[rightPointer] = tmp;
+                }
+                else
+                {
+                    leftPointer--;
+                    rightPointer--;
+                }
+            }
+        }
+    }
+}
